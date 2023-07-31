d97cdc19efb36f43aaa7d27fe3dd95197e07a6b8
Apple <apple@unwire-01421.unwire.dk>
2023-07-31 11:30:30 +0530

    Added comments

--- RandomImage/RandomImage/ViewController.swift
+++ RandomImage/RandomImage/ViewController.swift
@@ -2,22 +2,26 @@
 //  ViewController.swift
 //  RandomImage
 //
-//  Created by Apple on 30/07/23.
+//  Created by UMAR FAROOK M on 30/07/23.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
     
+    // CREATE THE IMAGE VIEW AND ITS STYLES
     private let imageView: UIImageView = {
         let imageView = UIImageView();
+        //STYLES
         imageView.contentMode = .scaleAspectFill
         imageView.backgroundColor = .white
         return imageView
     }()
     
+    // CREATE THE BUTTON AND ITS STYLES
     private let button: UIButton = {
         let button = UIButton();
+        //STYLES AND BUTTON LABEL
         button.backgroundColor = .white
         button.setTitle("Random Image", for: .normal)
         button.setTitleColor(.black, for: .normal)
@@ -26,22 +30,30 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // SET BACKGROUND COLOR FOR THE HOME SCREEN
         view.backgroundColor = .systemMint
+        // ADDED IMAGE VIEW IN MAIN MAIN VIEW
         view.addSubview(imageView)
         imageView.frame = CGRect(x:0, y:0, width: 300, height: 300)
         imageView.center = view.center
+        // ADDED BUTTON IN MAIN MAIN VIEW
         view.addSubview(button)
-        getRandomImage()
+        // ATTACHED THE TOUCH LISTENER TO THE BUTTON
         button.addTarget(self, action: #selector(didTapButton), for: .touchUpInside)
+        // CALL THE IMAGE API FOR FIRST TIME
+        getRandomImage()
+        
     }
     
+    // LISTENER METHOD
     @objc func didTapButton(){
         getRandomImage()
     }
     
+
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
+        // SET THE BUTTON POSITION
         button.frame = CGRect(x: 40,
                               y: view.frame.size.height-130-view.safeAreaInsets.bottom,
                               width: view.frame.size.width-80,
@@ -49,6 +61,7 @@ class ViewController: UIViewController {
         
     }
     
+    // DO THE API CALL AND SET THE IMAGE DATA INTO IMAGE VIEW
     func getRandomImage(){
         let urlString = "https://source.unsplash.com/random/600x600"
         let url = URL(string: urlString)!
